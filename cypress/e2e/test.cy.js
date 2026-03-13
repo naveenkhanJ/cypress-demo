5659c0867c66052f124dceae70d1558d227e26e8
naveenkhanJ <it23812826@my.sliit.lk>
2026-03-13 11:50:09 +0530

    changed the line

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,13 +1,13 @@
 describe('Demo Test', () => {
 
   it('Visits page and clicks button', () => {
-
+    
     cy.visit('http://localhost:8080')
-
+    
     cy.contains('Click Me').click()
-
-    cy.contains('Button Clicked')
-
+    
+    cy.contains('Button Clicked').should('be.visible')    
+    
   })
 
 })
